cfb6ad15f8aa5a0134a3778aaaec0aa7d8b704f6
Brian K Hines <briankhines@gmail.com>
2019-07-29 20:54:10 -0400

    Added font-awesome, added beer icon to nav

--- pubpalweb/src/app/app.module.ts
+++ pubpalweb/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { PubpalinterceptorService } from './providers/pubpalinterceptor.service';
 import { UserComponent } from './features/user/user.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -21,7 +22,8 @@ import { HomeComponent } from './features/home/home.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule // free font awesome icons: https://fontawesome.com/icons?d=gallery
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: PubpalinterceptorService, multi: true }
