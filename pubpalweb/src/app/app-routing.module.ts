8c91161c0c851dae19a4050baf08942d2403447c
Brian K Hines <briankhines@gmail.com>
2019-07-29 20:36:08 -0400

    Added home component, changed default routing Added navbar component, added to app.html

--- pubpalweb/src/app/app-routing.module.ts
+++ pubpalweb/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './features/user/user.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'user', component: UserComponent },
-  { path: '', component: UserComponent }
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
--- pubpalweb/src/app/app.module.ts
+++ pubpalweb/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { PubpalinterceptorService } from './providers/pubpalinterceptor.service';
 import { UserComponent } from './features/user/user.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +27,6 @@ import { UserComponent } from './features/user/user.component';
     { provide: HTTP_INTERCEPTORS, useClass: PubpalinterceptorService, multi: true }
   ],
   bootstrap: [AppComponent],
-  entryComponents: [UserComponent]
+  entryComponents: [UserComponent, HomeComponent]
 })
 export class AppModule { }
